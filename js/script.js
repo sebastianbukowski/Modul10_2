84789feb5c2c7232b904e0792fa688d0c8d0b8c0
sebastianbukowski <seb-27@o2.pl>
2019-01-01 21:51:41 +0100

    Add map,markers and map's centering button

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+'use strict';
 var slides = document.querySelector('.main-carousel');
 console.log('slides => ',slides);
 var template = document.querySelector('#templateSlide').innerHTML;
@@ -37,3 +38,37 @@ flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
+
+// GOOGLE MAPS
+
+(function(){
+  var infos = document.getElementById('infos');
+
+  	window.initMap = function() {
+		var map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 4,
+			center: table[0].coords
+		});
+    var markers = [];
+    for(var i=0; i<table.length; i++){
+      console.log(table[i]);
+      var marker = new google.maps.Marker({
+        position: table[i].coords,
+        map: map
+      });
+      markers.push(marker);
+
+      marker.addListener('click', function(){
+        console.log(markers);
+        infos.innerHTML = 'You clicked marker';
+      });
+
+    console.log('markers  at the end of loop =>', markers);
+  }
+
+  document.getElementById('center-map').addEventListener('click', function(event){
+      event.preventDefault();
+      map.panTo(table[0].coords);
+  });
+}
+})();
